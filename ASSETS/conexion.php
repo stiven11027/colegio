947d20deff71ab0253d0040af4ea763830979a7c
stiven11027 <stivencifuentes1887@gmail.com>
2026-03-20 01:47:16 +0000

    19 mar 2026, 20:47

---
+++ ASSETS/conexion.php
@@ -0,0 +1,11 @@
+<?php
+$conexion = mysqli_connect('127.0.0.1','root','','colegio_rural');
+if(!$conexion)
+{
+    echo "Error al conectar la base de datos";
+}
+else
+{
+    //echo "Si funciona";
+}
+?>
